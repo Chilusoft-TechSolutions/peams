066cb60651ffc4b2930920bc7b01f540fb16713f
chilusoft <mukukachilu@gmail.com>
2024-04-08 23:11:50 +0200

    Added postgresql backend

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -128,9 +128,17 @@ WSGI_APPLICATION = "backend.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    # "default": {
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": BASE_DIR / "db.sqlite3",
+    # }
+    "default":{
+        "ENGINE": "django.db.backends.postgresql",
+        'NAME': 'catalogue',
+        'USER': 'postgres',
+        'PASSWORD': '321@test',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
